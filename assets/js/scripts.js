75e4beb95d2cee26caa006b45e9363174baece60
froelen <marius.trouillon@proton.me>
2026-02-11 19:28:55 +0100

    comment getEngine function and run it by default

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,9 @@
-function getEngine() {
+function getEngine() { // returns a string of the browser's engine bypassing its userAgent string
     const docStyle = document.documentElement.style;
-    if (window.navigator.userAgentData) return 'Blink';
+    if (window.navigator.userAgentData) return 'Blink'; // only present in Chromium
     if ('MozAppearance' in docStyle || window.netscape) return 'Gecko';
-    if ('WebkitAppearance' in docStyle && !window.chrome) return 'WebKit';
+    if ('WebkitAppearance' in docStyle && !window.chrome) return 'WebKit'; // Chromium engine would return true to Webkit test, thus adding not window.chrome
     return 'Blink'; // default to Chromium engine
 };
+
+document.body.classList.add(getEngine().toLowerCase()); // adds as a class the browser engine for easy specific fixes in CSS 
