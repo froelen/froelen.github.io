f765748fead42a1f868cddf3848fc962861ca043
froelen <froelen@tuta.io>
2026-03-29 00:38:54 +0100

    Update sripts scructure for instant color scheme

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,5 @@
-const body = document.body;
+const html = document.documentElement;
+
 
 // Browser engine handler
 function getEngine() {  // returns a string of the browser's engine bypassing its userAgent string
@@ -8,14 +9,26 @@ function getEngine() {  // returns a string of the browser's engine bypassing it
     if ('WebkitAppearance' in docStyle && !window.chrome) return 'WebKit';  // Chromium engine would return true to Webkit test, thus adding not window.chrome
     return 'Blink';  // default to Chromium engine
 };
+html.classList.add(getEngine().toLowerCase());  // adds as a class the browser engine for easy specific fixes in CSS 
 
 
-body.classList.add(getEngine().toLowerCase());  // adds as a class the browser engine for easy specific fixes in CSS 
-
-// Color scheme handler
-function toggleScheme() { body.classList.toggle('dark'); };
+// Color scheme handler (page load toggle in scheme-init.js)
+const metaTheme = document.querySelector('meta[name="theme-color"]');
 const colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
-if (colorSchemeQuery.matches) { toggleScheme(); };  // Initial toggle
-colorSchemeQuery.addEventListener('change', toggleScheme);  // When browser preference changes
-document.getElementById("color-scheme-button").addEventListener('click', () => { toggleScheme(); });  // Handles scheme button clicks
+function toggleScheme() {
+    html.classList.toggle('dark');
+    metaTheme.setAttribute('content', html.classList.contains('dark') ? '#181952' : '#F7F8FF');
+}
+function checkSchemeSync() {
+    if ( (colorSchemeQuery.matches && !html.classList.contains('dark')) || (!colorSchemeQuery.matches && html.classList.contains('dark'))) toggleScheme();
+}
+
+colorSchemeQuery.addEventListener('change', checkSchemeSync);
+document.getElementById('color-scheme-button').addEventListener('click', toggleScheme);
+
+
+// Toggles nav-lang and nav-links visibility from menu button
+document.getElementById('nav-button-collapse').addEventListener('click', () => {
+    document.getElementById('header').classList.toggle('open');
+});
